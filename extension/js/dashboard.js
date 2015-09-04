1661c9cbd037397a6d4b2e7aab0043eaaa47263c
Chase Hawthorne <ch73168@gmail.com>
2015-09-04 16:14:13 -0600

    Working on vid streaming

--- extension/js/dashboard.js
+++ extension/js/dashboard.js
@@ -1,15 +1,28 @@
+var streamURL = null;
+var video = null;
+
 window.onload = function(){
-    console.log($("#video"));
+    video = $("#rtcvid").get(0);
     
+    $("#start-stream").click(beginStream);
+    $("#start-stream").click(endStream);
+};
+
+var beginStream = function(){
     navigator.webkitGetUserMedia({video: true}, 
     function(stream){
         console.log("Media acquired.");
         
-        $("#rtcvid").attr('src', window.URL.createObjectURL(stream));
+        streamURL = window.URL.createObjectURL(stream)
+        video.src = streamURL;
     },
     function(e){
         console.log("Unable to access media.", e);
     });
+};
+
+var endStream = function(){
+    
 };
 
 var setupStyleListeners = function(){
